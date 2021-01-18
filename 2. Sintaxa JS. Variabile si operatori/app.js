f7d93a5fc1b697029ef936c4854f7a5426f46bcd
Sergiu Chilat <chilatsergiu@gmail.com>
2021-01-18 14:42:50 +0200

    Variabile si operatori

---
+++ 2. Sintaxa JS. Variabile si operatori/app.js
@@ -0,0 +1,28 @@
+let number = 5;
+console.log(number);
+number = 3;
+console.log(number);
+const pi = 3.14;
+
+
+/* Operatori aritmetici */
+let a = 4;
+let b = 0;
+
+console.clear();
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+console.clear();
+let x = "2";
+let y  = "5a";
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+
+console.clear();
+console.log(2 ** 3);
