48415dadb1c657001ac81eaa001e4d5506832038
Sergiu Chilat <chilatsergiu@gmail.com>
2021-01-19 16:57:22 +0200

    Prelucrarea arborelui DOM. QuerySelector

--- 4. Obiecte/app.js
+++ 4. Obiecte/app.js
@@ -11,3 +11,13 @@ document.getElementById("personName").innerHTML = person.name;
 document.getElementById("personPhone").innerHTML = person.contacts.phone;
 document.getElementById("personEmail").innerHTML = person.contacts.email;
 console.log(person);
+
+const number = 123;
+
+document.querySelectorAll(".element").forEach(el => el.innerHTML = number);
+
+let i = 10;
+document.querySelectorAll(".elementNumber").forEach(el => {
+    el.innerHTML = i;
+    i--;
+})
