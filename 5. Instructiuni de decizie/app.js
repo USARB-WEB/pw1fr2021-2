af060a0925dab32f852ca84f4e5b3a9ce3f8ec33
Sergiu Chilat <chilatsergiu@gmail.com>
2021-01-19 17:14:50 +0200

    Instructiuni de decizie. IF, Switch case

---
+++ 5. Instructiuni de decizie/app.js
@@ -0,0 +1,72 @@
+/*
+const age = prompt("Ce virsta ai?");
+if(age >= 7 && age <= 19) {
+    alert("Esti elev");
+} else if(age >= 20 && age <= 30) {
+    alert("Esti student");
+} else {
+    alert("Nu esti elev");
+}
+*/
+/*
+const digit = 0;
+if(digit === 0) {
+    console.log("Zero");
+} else if(digit === 1) {
+    console.log("Zero");
+} else if(digit === 2) {
+    console.log("Zero");
+} else if(digit === 3) {
+    console.log("Zero");
+} else if(digit === 4) {
+    console.log("Zero");
+} else if(digit === 5) {
+    console.log("Zero");
+} else if(digit === 6) {
+    console.log("Zero");
+} else if(digit === 7) {
+    console.log("Zero");
+} else if(digit === 8) {
+    console.log("Zero");
+} else if(digit === 9) {
+    console.log("Zero");
+} else {
+    console.log("Nu este cifra");
+}
+
+switch(digit) {
+    case 0:
+        console.log("Zero");
+        break;
+    case 1:
+        console.log("Zero");
+        break;
+    case 2:
+        console.log("Zero");
+        break;
+    case 3:
+        console.log("Zero");
+        break;
+    case 4:
+        console.log("Zero");
+        break;
+    case 5:
+        console.log("Zero");
+        break;
+    case 6:
+        console.log("Zero");
+        break;
+    case 7:
+        console.log("Zero");
+        break;
+    case 8:
+        console.log("Zero");
+        break;
+    case 9:
+        console.log("Zero");
+        break;
+    default:
+        console.log("Zero");
+}
+
+*/
