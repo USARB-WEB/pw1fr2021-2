000dc6c8e7001bfdba029cf595f06ea58fec7506
Sergiu Chilat <chilatsergiu@gmail.com>
2021-01-19 17:26:25 +0200

    Instructiuni repetitive

---
+++ 6. Instructiuni repetitive/app.js
@@ -0,0 +1,18 @@
+console.log("For");
+for (let index = 0; index <= 5; index++) {
+    console.log(index);
+}
+
+console.log("While");
+let index = 0;
+while ( index <= 5) {
+    console.log(index);
+    index++;
+}
+
+console.log("Do While");
+index = 0;
+do {
+    console.log(index);
+    index++;
+}while ( index <= 5);
